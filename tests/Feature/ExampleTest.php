57bb7c265893625d9f62384eb253a6b7df3858a6
Michael Rivera <soymrmike@gmail.com>
2019-04-20 13:28:07 -0700

    First test for video creation.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,17 +5,44 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class VideosTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    use RefreshDatabase;
+    
+    public function testCanCreateVideo()
     {
-        $response = $this->get('/');
+        $response = $this->json('POST', '/videos', [
+            'link' => 'https://www.youtube.com/watch?v=AEF9ak24z8s'
+        ]);
 
-        $response->assertStatus(200);
+        $response
+            ->assertStatus(201)
+            ->assertJson([
+                'id' => 1
+            ]);
     }
+
+    public function testDoesNotDuplicateVideos()
+    {
+        $response = $this->json('POST', '/videos', [
+            'link' => 'https://www.youtube.com/watch?v=AEF9ak24z8s'
+        ]);
+
+        $response
+            ->assertStatus(201)
+            ->assertJson([
+                'id' => 1
+            ]);
+        
+        $response = $this->json('POST', '/videos', [
+            'link' => 'https://www.youtube.com/watch?v=AEF9ak24z8s'
+        ]);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'id' => 1
+            ]);
+    }
+    
 }
