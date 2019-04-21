162dcd5b1b2218aebc1ae8bc5d11cf5951747aa1
Michael Rivera <soymrmike@gmail.com>
2019-04-20 17:32:11 -0700

    Moving video display html to VideoSummary component.

--- resources/js/components/VideoSummary.vue
+++ resources/js/components/VideoSummary.vue
@@ -1,9 +1,46 @@
 <template>
 
-  <div>
-    <h3>{{ video.title }}</h3>
-    
-  </div>
+  <div class="row">
+
+    <div class="col" >
+
+      <h3>{{ video.title }} <a :href="vLink()" target="_blank"><i class="fab fa-youtube" title="Visit video" ></i></a></h3>
+
+        <div v-if="false" class="alert alert-info">
+          
+        </div>
+      
+        <div v-if="false"  class="alert alert-danger">
+          
+        </div>
+
+      <br/>
+
+      <strong>Video Owner</strong>: <a href="{{ route('channels.show', ['channel' => $video->channel_id]) }}">{{ $video->owner }}</a>
+      <a href="https://www.youtube.com/channel/{!! $video->channel_id !!}" target="_blank"><i class="fab fa-youtube" title="Visit channel"  ></i></a>
+      <br/>
+      <br/>
+
+      <strong>Views</strong>: {{ number_format($video->views) }}
+      <br/>
+      <strong>Likes</strong>: {{ number_format($video->likes) }}
+      <br/>
+      <strong>Dislikes</strong>: {{ number_format($video->dislikes) }}
+      <br/>
+      <strong>Like-to-dislike ratio</strong>: {{ number_format($video->likes / $video->dislikes, 2) }}
+      <br/>
+      <strong>Calculation</strong>: log_10({{ number_format($video->views) }}) * {{ number_format($video->likes / $video->dislikes, 2) }}
+      <a target="_blank" href="http://www.google.com/search?q={{ urlencode('log_10(' . $video->views . ') * (' . $video->likes . ' / ' . $video->dislikes . ')') }}">See calculation on <i class="fab fa-google" ></i> Google</a>.
+      <br/>
+      <strong>Score</strong>: {{ $video->score }}
+      
+
+      <br/>
+      <br/>
+      <a href="/">Video List</a>
+      
+    </div>
+  </div>    
 
 </template>
 
@@ -11,6 +48,9 @@
 export default {
   props: {
     video: Object
+  },
+  methods: {
+    vLink: function() { return "https://www.youtube.com/watch?v=" + this.video.vid }
   }
 }
 </script>
